5882f853b20307faee07ddb6ae5470b8e4ea53f5
Sharded <98332856+ShardedFilms@users.noreply.github.com>
2024-02-12 20:27:33 +0700

    Delete src/flame/special directory

--- src/flame/special/SpecialState.java
+++
@@ -1,9 +0,0 @@
-package flame.special;
-
-public abstract class SpecialState{
-    public void update(){}
-    public void draw(){}
-    public void init(){}
-    public void loadAssets(){}
-    public void loadClient(){}
-}
