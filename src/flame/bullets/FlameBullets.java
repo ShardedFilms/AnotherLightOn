1ebad0e76bae8906aaa00010e087f6957fa9fa4f
Sharded <98332856+ShardedFilms@users.noreply.github.com>
2024-03-29 22:18:37 +0700

    Update FlameBullets.java

--- src/flame/bullets/FlameBullets.java
+++ src/flame/bullets/FlameBullets.java
@@ -42,7 +42,96 @@ public class FlameBullets{
 
                         if(entity instanceof Unit unit){
                             if(impact) 
-                            Stage2.killUnit(unit);
+            EmpathyDamage.damageUnit(u, unit.maxHealth + 900000f, true, () -> {
+            float trueSize = Math.max(unit.hitSize, Math.min(unit.type.region.width * Draw.scl, unit.type.region.height * Draw.scl));
+
+            BloodSplatter.setLifetime(45f * 60);
+            BloodSplatter.explosion(40, unit.x, unit.y, trueSize / 2f, trueSize * 1.5f + 60f, trueSize * 0.4f / 4f + 12f);
+            BloodSplatter.explosion(60, unit.x, unit.y, trueSize / 2f, trueSize + 50f, trueSize * 0.75f / 4f + 25f);
+            BloodSplatter.setLifetime();
+
+            FragmentationBatch batch = FlameOut.fragBatch;
+            batch.baseElevation = 0f;
+            batch.fragFunc = e -> {
+                float dx = (e.x - unit.x) / 35f;
+                float dy = (e.y - unit.y) / 35f;
+
+                e.vx = dx;
+                e.vy = dy;
+                e.vr = Mathf.range(2f);
+                //e.lifetime = 180f;
+                e.vz = Mathf.random(-0.01f, 0.1f);
+
+                e.lifetime = 25f * 60f;
+            };
+            batch.fragDataFunc = f -> {
+                f.fadeOut = true;
+                f.trailEffect = Fx.none;
+            };
+            batch.onDeathFunc = null;
+            batch.altFunc = (x, y, r) -> {};
+            batch.trailEffect = batch.explosionEffect = null;
+            batch.fragColor = Color.white;
+
+            batch.switchBatch(u::draw);
+
+            float size = (trueSize / 2f) / 20f;
+            float size2 = (trueSize / 2f);
+
+            int amount = 3 + (int)(size * size);
+            int amount2 = Mathf.random(3, 6);
+            for(int i = 0; i < amount2; i++){
+                Vec2 v = Tmp.v1.trns(Mathf.random(360f), Mathf.random());
+
+                Part2 p = new Part2();
+                p.x = unit.x + v.x * (size2 / 3f);
+                p.y = unit.y + v.y * (size2 / 3f);
+                p.rotation = p.targetRotation = Mathf.random(360f);
+                p.bend = p.targetBend = Mathf.random(25f, 90f);
+
+                p.vx = v.x * size2 / 10f;
+                p.vy = v.y * size2 / 10f;
+                p.vr = Mathf.range(3f);
+
+                p.length = size2 * Mathf.random(0.7f, 1.1f);
+                p.width = (p.length / 4f) * Mathf.random(1f, 1.5f);
+
+                p.add();
+            }
+
+            for(int i = 0; i < amount; i++){
+                Vec2 v = Tmp.v1.trns(Mathf.random(360f), Mathf.random());
+
+                Part p = new Part();
+                p.x = unit.x + v.x * size2;
+                p.y = unit.y + v.y * size2;
+                p.rotation = Mathf.random(360f);
+
+                p.vx = v.x * size2 / 4.5f;
+                p.vy = v.y * size2 / 4.5f;
+                p.vr = Mathf.range(6f);
+                p.hitSize = (size2 / 65f) * Mathf.random(0.9f, 1f);
+
+                p.add();
+            }
+
+            Part cage = new Part();
+            cage.x = unit.x;
+            cage.y = unit.y;
+            cage.rotation = Mathf.random(360f);
+
+            Vec2 v = Tmp.v1.trns(Mathf.random(360f), Mathf.random());
+
+            cage.vx += v.x * size2 / 8f;
+            cage.vy += v.y * size2 / 8f;
+            cage.vr = Mathf.range(6f);
+            cage.hitSize = (size2 / 60f) * Mathf.random(0.9f, 1f);
+
+            cage.textureIdx = -1;
+
+            cage.add();
+        });
+    }
                         };
                     }            
                                                       };
